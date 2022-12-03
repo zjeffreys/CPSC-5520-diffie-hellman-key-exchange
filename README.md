2734a2d6d2ad714da3093f08385552a231e54e38
zjeffreys <zjeffreys@seattlu.edu>
2022-12-02 22:23:33 -0800

    works other than the algo for x1 mod x2

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CPSC-5520-diffie-hellman-key-exchange
+
+Diffie Hellman Extra credit lab – key exchange
+Use Python to implement the Diffie Hellman key exchange between two nodes. Each node should generate a random number at the beginning of the program to be used as x and y in the algorithm. The first node, Node Bob, should be created and wait for a response from another node. The second node, node Alice, should be started with the address to the first node. When the second node , node Alice, begins it should send a message to Node (Bob) with (n, g, and g^x mod n). After receiving a message from the Alice, node 1 (Node Bob) should receive the message and raise the value of the key to the power of y, the random variable established at the beginning of the node. Node Bob w ill store this value for future communication. Following storing the symmetrical key, Node (Bob) should calculate g^y mod n and send it back to Node Alice. After receiving the response message back from Node Bob, Node Alice should multiple the g^y mod n by its x to obtain the shared key between Alice and Bob.
+After receiving the message both nodes should print out the values of their new symmetrical key and print it to console to show how both nodes have the same shared key after the key exchange.
+Requirements:
+• Node Alice should communicate with Node Bob with the message n, g, g^x mod n.
+• Node Bob should compute the shared key by raising it to the power of y.
+• Node Bob should send a message containing g^y mod n back to Node Alice
+• Node Alice should compute the shared key by raising it to the power of x
+• Both Alice and Bob should print out the same number after they communicate
+• Should use TCP/IP to stream messages back and forth.
---
+++ lab6.py
@@ -0,0 +1,97 @@
+import random
+import socket
+import os, sys
+import pickle
+import threading
+import time
+
+ONE_SEC = 1
+HOST = '127.0.0.1' #for simplicity assume on same host
+DEFAULT_ADDRESS = (HOST, 0)
+BUFFER = 2048
+TEST_PORT = 50001
+N = 100_000
+G = 48
+
+
+class Node(object):
+    def __init__(self, name, my_port, peer_port = None): 
+        # create a private key, using small number for simplicity
+        
+        self.my_name = name
+        self.peer_port = peer_port
+        self.my_port = my_port
+        self.x = random.randint(1,9)
+        self.myAddr = None  # listener sets port
+
+    def run(self):
+        """starts a listener threat to here incoming messages"""
+        listener = threading.Thread(target=self.listen_to_publisher)   
+        listener.start()
+        time.sleep(ONE_SEC)  # give lister time to set port dynamically 
+        if self.peer_port != None:
+            subscriber = threading.Thread(target=self.subscribe_to_key_exchange)
+            subscriber.start()
+        
+        
+    
+    
+    def listen_to_publisher(self): 
+        """Creates a listener socket """
+        print("Node", self.my_name, "is listening for updates")
+
+        listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        listeningAddress = (HOST, int(self.my_port))
+        listener.bind(listeningAddress)
+        while True:
+            # wait to receive message
+            data, addr = listener.recvfrom(BUFFER)
+            unpickeledData = pickle.loads(data)
+            print("Listener( ",self.my_name,") Recieved Message: ", unpickeledData)
+            val = int(data[2]) ** self.x
+            print("Final Value From", self.my_name, ":", val)
+
+
+            # send message back
+            # msg = (unpickeledData[1], unpickeledData[2], self.get_key())
+            my_data = pickle.dumps(self.get_key())
+            listener.sendto(my_data, addr)
+            
+            
+    
+    def get_key(self, n = N, g=G):
+        """return (n, g, and g^x mod n)"""
+        num = g**self.x
+        num = num % n         
+        return (n, g, num)
+        
+    def print_final_number(self, data, n=N, g=G):
+        return int(data[2]) ** self.x
+        
+
+
+    def subscribe_to_key_exchange(self):
+        # while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+            key = self.get_key()
+            data = pickle.dumps(key)
+            peer_address = (HOST, int(self.peer_port))
+            s.sendto(data, peer_address)
+            data, address = s.recvfrom(BUFFER)
+            my_data = pickle.loads(data)
+            print("Final Number(", self.my_name, "): ", self.print_final_number(my_data))
+                
+            
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        node = Node("Bob", sys.argv[1])
+        node.run()
+
+    elif len(sys.argv) == 3:
+        node = Node("Alice", sys.argv[1], sys.argv[2])
+        node.run()
+
+    else: 
+        print("Usage: python lab6.py my_port peer_port_or_none")
